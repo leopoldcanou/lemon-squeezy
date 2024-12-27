70438e5b125188ccf253a37aef363c73e0722ea3
Léopold Canou <52009517+leopoldcanou@users.noreply.github.com>
2024-12-27 20:03:56 +0100

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,29 @@
+import taxCompliance from "../../public/tax-compliance.jpg";
+import saasPayments from "../../public/saas-payments.jpg";
+import instantPayment from "../../public/instant-payment.jpg";
+import currency from "../../public/currency.jpg";
+import fraud from "../../public/fraud.jpg";
+import paymentRecovery from "../../public/payment-recovery.jpg";
+
 import Header from "@/components/header";
 import SimpleText from "@/components/simpleText";
+import InteractiveFeatures from "@/components/interactiveFeatures";
+import { Feature } from "@/components/interactiveFeatures";
+import featuresData from "@/app/features.json";
+
+const imageMapping = {
+  taxCompliance,
+  saasPayments,
+  instantPayment,
+  currency,
+  fraud,
+  paymentRecovery,
+};
+
+const features: Feature[] = featuresData.map((feature) => ({
+  ...feature,
+  imageSrc: imageMapping[feature.imageSrc as keyof typeof imageMapping],
+}));
 
 export default function Home() {
   return (
@@ -13,6 +37,7 @@ export default function Home() {
               multi-currency support, failed payment recovery, PayPal integration and
               more. We make running your software business easy peasy."
       />
+      <InteractiveFeatures features={features} />
     </>
   );
 }
