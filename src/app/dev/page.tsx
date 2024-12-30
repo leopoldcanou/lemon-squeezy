9908dd83aad03482fec33f5fa991ac602ad3e95e
Léopold Canou <52009517+leopoldcanou@users.noreply.github.com>
2024-12-30 01:47:02 +0100

    remove dev route

--- src/app/dev/page.tsx
+++
@@ -1,8 +0,0 @@
-import FeatureGrid, { FeatureGridInterface } from "@/components/featuresGrid";
-import featuresGridData from "@/app/firstFeaturesGrid.json";
-
-const features: FeatureGridInterface[] = featuresGridData;
-
-export default function Page() {
-  return <FeatureGrid idColor="#D50B3E" features={features} />;
-}
